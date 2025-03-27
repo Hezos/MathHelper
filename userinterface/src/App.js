55a7e0ac76471d64cc43570b379fa1583382e2d2
Hezos <nbence0620@gmail.com>
2025-03-27 20:09:16 +0100

    Added React UI

---
+++ userinterface/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Learn math with my sidekick!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
