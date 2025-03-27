2e1b4b6f11f54fad91b8fad616e7142e108f2bc2
Hezos <nbence0620@gmail.com>
2025-03-27 20:58:27 +0100

    Stuck at figuring out a way for animations.

--- userinterface/src/App.js
+++ userinterface/src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+
+
+//const [data, setData] = useState();
+
+var data = {
+  meta: 'This is metadata',
+  xPos:10
+};
+
+function frame(){
+  setInterval(() =>{
+    data.xPos += 10;
+    console.log('Moving object');
+  },10);
+}
 
 function App() {
+  
+  frame();
+
+
   return (
     <div className="App">
       <h1>
         Learn math with my sidekick!
       </h1>
+      <p>
+        {data.meta}
+      </p>
+      <svg width="400" height="120">
+        <rect x={data.xPos.toString()} y={data.xPos.toString()} width={data.xPos.toString()}
+         height="100" stroke="red" strokeWidth="6" fill="blue" />
+      </svg>
     </div>
   );
 }
