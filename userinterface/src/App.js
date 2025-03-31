a35dac8c16d77ad3afdde36941daf29bec61e2de
Hezos <nbence0620@gmail.com>
2025-03-31 20:01:41 +0200

    Added smaller rectangle.

--- userinterface/src/App.js
+++ userinterface/src/App.js
@@ -7,6 +7,13 @@ function RotateElement(id, degree){
   document.getElementById(id).style.transform = `rotateZ(${degree}deg)`;
 }
 
+function PositionHolderX(xValue, mainRectWidth, degree){
+  return Math.cos(degree) * (xValue-mainRectWidth);
+}
+
+function PositionHolderY(xValue, mainRectWidth, degree){
+  return Math.sin(degree) * (xValue-mainRectWidth);
+}
 
 function App() {
   
@@ -26,13 +33,27 @@ function App() {
       <svg class="border border-4" width="100%" height={window.innerHeight * 0.9} >
         <rect style={{'transform-origin': 'center center'}} id='myRect' x={window.innerWidth/2 - window.innerWidth/10} 
         y={window.innerHeight*0.9/2} width={window.innerWidth/5} 
-        height={window.innerHeight*0.03} fill="blue" transform={`rotateZ(${count}deg)`} />
+        height={window.innerHeight*0.03} fill="blue"  />
+        <rect x={window.innerWidth/2 - window.innerWidth/10 + PositionHolderX(window.innerWidth/2 - window.innerWidth/10,window.innerWidth/10,count + 30)} 
+        y={window.innerHeight*0.9/2 + PositionHolderY(window.innerHeight*0.9/2,window.innerWidth/10,count + 30)} width={window.innerWidth/20} 
+        height={window.innerHeight*0.02} fill="yellow"/>
       </svg>  
       <button onClick={() => {
+          //Set a different rotating sequence
+          setCount(count - 30);  
+          RotateElement("myRect", count);
+        }}>
+        Add to left
+      </button>
+      <button>
+        Restart balance
+      </button>
+      <button onClick={() => {
+          //Set a different rotating sequence
           setCount(count + 30);  
           RotateElement("myRect", count);
         }}>
-        Rotate balance
+        Add to right
       </button>
     </div>
   );
