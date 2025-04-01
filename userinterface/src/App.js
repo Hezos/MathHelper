7cd5f55e642790f573cff3efce648594370d2e28
Hezos <nbence0620@gmail.com>
2025-04-01 15:54:22 +0200

    Started using framer-motion for animatiions.

--- userinterface/src/App.js
+++ userinterface/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React, { useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
+import { motion } from 'framer-motion';
 
 function RotateElement(id, degree){
-  document.getElementById(id).style.transform = `rotateZ(${degree}deg)`;
+  document.getElementById(id).style.transform = `rotate(${degree} ${window.innerWidth/2} ${window.innerHeight * 0.9/2})`;
 }
 
 function PositionHolderX(xValue, mainRectWidth, degree){
@@ -13,6 +13,28 @@ function PositionHolderX(xValue, mainRectWidth, degree){
 
 function PositionHolderY(xValue, mainRectWidth, degree){
   return Math.sin(degree) * (xValue-mainRectWidth);
+}
+
+function Draw(){
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.save();
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight*0.9);
+    //Place element in center
+    ctx.translate(window.innerWidth/2, window.innerHeight*0.9 /2);
+    const now = new Date();
+    const sec = now.getSeconds();
+    ctx.rotate((sec*Math.PI)/30);  
+    ctx.strokeStyle = "#D40000";
+    ctx.fillStyle = "#D40000";
+    ctx.beginPath();
+    ctx.rect(window.innerWidth/10*-1,window.innerHeight*0.9 /80*-1, window.innerWidth/5, window.innerHeight*0.9 /40);
+    ctx.stroke();
+    ctx.fill();
+    ctx.restore();
+
+    //window.requestAnimationFrame(Draw(count));
+  
 }
 
 function App() {
@@ -25,23 +47,28 @@ function App() {
   }, 1000);
 
   //Change the rectanlge to a component with parameters
+
+/*
+      <canvas id="canvas" width={window.innerWidth} height={window.innerHeight*0.9} class="border border-2"></canvas>
+
+*/
+
   return (
     <div className="App">
       <h1>
         Learn math with my sidekick!
       </h1>
-      <svg class="border border-4" width="100%" height={window.innerHeight * 0.9} >
-        <rect style={{'transform-origin': 'center center'}} id='myRect' x={window.innerWidth/2 - window.innerWidth/10} 
-        y={window.innerHeight*0.9/2} width={window.innerWidth/5} 
-        height={window.innerHeight*0.03} fill="blue"  />
-        <rect x={window.innerWidth/2 - window.innerWidth/10 + PositionHolderX(window.innerWidth/2 - window.innerWidth/10,window.innerWidth/10,count + 30)} 
-        y={window.innerHeight*0.9/2 + PositionHolderY(window.innerHeight*0.9/2,window.innerWidth/10,count + 30)} width={window.innerWidth/20} 
-        height={window.innerHeight*0.02} fill="yellow"/>
-      </svg>  
+      <motion.div
+      animate={{
+      rotate:180,
+      transition: { duration: 1 }
+      }} >
+    <p>Sample</p>
+      </motion.div>
       <button onClick={() => {
           //Set a different rotating sequence
-          setCount(count - 30);  
-          RotateElement("myRect", count);
+          setCount(count - 30);
+          window.requestAnimationFrame(Draw);
         }}>
         Add to left
       </button>
@@ -51,7 +78,7 @@ function App() {
       <button onClick={() => {
           //Set a different rotating sequence
           setCount(count + 30);  
-          RotateElement("myRect", count);
+          window.requestAnimationFrame(Draw);
         }}>
         Add to right
       </button>
