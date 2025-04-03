6690036f360842cf211f03139f509224b2c7e4ae
Hezos <nbence0620@gmail.com>
2025-04-03 15:47:00 +0200

    Changed animations to canvas

--- userinterface/src/App.js
+++ userinterface/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import { motion } from 'framer-motion';
+//import { motion } from 'framer-motion';
 
 /*
 function PositionHolderX(xValue, mainRectWidth, degree){
@@ -15,7 +15,32 @@ function PositionHolderY(xValue, mainRectWidth, degree){
 */
 
 function App() {
+
   
+function DrawBasic(posX, posY, width, height){
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  //drawing the rectangle
+  ctx.fillStyle = "gray";
+  ctx.fillRect(posX-width/2, posY-height/2, width, height);
+}
+
+//If there wasn't any reset it continues rotating over and over again.
+function DrawRotated(posX, posY, width, height){
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  // Matrix transformation
+  ctx.translate(posX, posY);
+  ctx.rotate(Math.PI / 12);
+  ctx.translate(-1*(posX), -1*(posY));
+
+  // Rotated rectangle
+  ctx.fillStyle = "red";
+  ctx.fillRect(posX-width/2, posY-height/2, width, height);
+
+}
+
   const [count, setCount] = useState(0);
 
   setInterval(() => {
@@ -26,28 +51,42 @@ function App() {
   //Change the rectanlge to a component with parameters
 
 /*
-      <canvas id="canvas" width={window.innerWidth} height={window.innerHeight*0.9} class="border border-2"></canvas>
+  
 
 */
+//https://motion.dev/docs/react-quick-start
+/*
 
-  return (
-    <div className="App">
-      <h1>
-        Learn math with my sidekick!
-      </h1>
       <motion.div
           style={{width:window.innerWidth, height:window.innerHeight*0.9}}
           animate={{
             rotate:count,
             transition: { duration: 0 }
           }} >  
-        <svg style={{width:window.innerWidth, height:window.innerHeight*0.9}}>
+        <svg  style={{width:window.innerWidth, height:window.innerHeight*0.9}}>
           <rect width={`${window.innerWidth/5}`} height={`${window.innerHeight/40}`} x={`${window.innerWidth/2-window.innerWidth/10}`}
           y={`${window.innerHeight*0.9/2 - window.innerHeight/80}`} rx={`${window.innerWidth*0.005}`} ry={`${window.innerHeight*0.005}`}
           fill="blue" />
         </svg>
       </motion.div>
+      <svg class="position-absolute" style={{width:window.innerWidth, height:window.innerHeight*0.9}}>
+      <rect width={`${window.innerWidth/10}`} height={`${window.innerHeight/20}`} x={`${window.innerWidth/2-window.innerWidth/10}`}
+          y={`${window.innerHeight*0.9/2 - window.innerHeight/80}`} rx={`${window.innerWidth*0.005}`} ry={`${window.innerHeight*0.005}`}
+          fill="red" />
+      </svg>
 
+
+
+*/
+
+
+  return (
+    <div className="App" >
+      <h1>
+        Learn math with my sidekick!
+      </h1>
+      <canvas id="canvas" width={window.innerWidth} height={window.innerHeight*0.9} class="border border-2"></canvas>
+      <div >
       <button onClick={() => {
           //Set a different rotating sequence
           setCount(count - 30);
@@ -55,16 +94,21 @@ function App() {
         }}>
         Add to left
       </button>
-      <button>
+      <button onClick={()=>{
+        DrawBasic(window.innerWidth / 2,window.innerHeight*0.9/2, window.innerWidth/5,window.innerHeight/40);
+      }}>
         Restart balance
       </button>
       <button onClick={() => {
           //Set a different rotating sequence
-          setCount(count + 30);  
+          setCount(count + 30);
+          DrawRotated(window.innerWidth / 2,window.innerHeight*0.9/2, window.innerWidth/5,window.innerHeight/40);  
           //window.requestAnimationFrame(Draw);
         }}>
         Add to right
       </button>
+      </div>
+      
     </div>
   );
 }
