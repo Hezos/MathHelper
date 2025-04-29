e79ef6e71cad1983814dbe97883197565501e078
Nagy Bence <nbence0620@gmail.com>
2025-04-29 19:32:06 +0200

    Added advertisement component but doesn't seem to show.

--- userinterface/src/components/Advertisement.js
+++ userinterface/src/components/Advertisement.js
@@ -1,12 +1,22 @@
-
+import React from 'react';
+import AdSense from 'react-adsense';
 
 export function Advertisement(){
+//            
+
     return (
         <div>
             <p>
                 Placeholder for applications and advertisement
             </p>
-            <p>Placeholder for advertisement</p>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <AdSense.Google
+            client='ca-pub-7292810486004926'
+            slot='7806394673'
+            style={{ display: 'block' }}
+            layout='in-article'
+            format='fluid'
+        />
 
         </div>        
     );
