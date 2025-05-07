d5315f25c1c1280c63df97981b757416330dbfdd
Nagy Bence <nbence0620@gmail.com>
2025-05-07 13:33:09 +0200

    Changed text family on advertisement.

--- userinterface/src/components/Advertisement.js
+++ userinterface/src/components/Advertisement.js
@@ -8,18 +8,26 @@ export function Advertisement(){
             <p>
                 Placeholder for applications and advertisement
             </p>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4527385682541454"
-                crossorigin="anonymous"></script>
-            <ins class="adsbygoogle"
-            style={{display:'block'}}
-     data-ad-format="autorelaxed"
-     data-ad-client="ca-pub-4527385682541454"
-     data-ad-slot="2714714081"></ins>
+            <body>
+    <h2 style={{ textAlign: "center", fontFamily:"sans-serif"}}>Advertisement</h2>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4527385682541454"
+    crossorigin="anonymous"></script>
+    <div style={{display: "flex",
+    justifyContent: "center"}} >
+      <ins class="adsbygoogle"
+      style={{display: "block", width: "75%", borderStyle: "dotted"}}
+      data-ad-format="autorelaxed"
+      data-ad-client="ca-pub-4527385682541454"
+      data-ad-slot="2714714081"></ins>
+      
+    </div>
 <script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
+(adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 
-        </div>        
+  </body>
+
+   </div>      
     );
 }
 
