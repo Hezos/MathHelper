8ba6f8250d27fc78d5f9fa1b35cd8b2a5f2e7390
Nagy Bence <nbence0620@gmail.com>
2025-05-10 11:32:28 +0200

    Removed google ads, they made loading pages super slow.

--- userinterface/src/components/Advertisement.js
+++ userinterface/src/components/Advertisement.js
@@ -1,32 +1,13 @@
 import React from 'react';
 
 export function Advertisement(){
-//            
 
     return (
         <div>
             <p>
                 Placeholder for applications and advertisement
             </p>
-            <body>
-    <h2 style={{ textAlign: "center", fontFamily:"sans-serif"}}>Advertisement</h2>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4527385682541454"
-    crossorigin="anonymous"></script>
-    <div style={{display: "flex",
-    justifyContent: "center"}} >
-      <ins class="adsbygoogle"
-      style={{display: "block", width: "75%", borderStyle: "dotted"}}
-      data-ad-format="autorelaxed"
-      data-ad-client="ca-pub-4527385682541454"
-      data-ad-slot="2714714081"></ins>
-      
-    </div>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
-  </body>
-
+            
    </div>      
     );
 }
