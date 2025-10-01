1cbcf6661ec33429404fd1e5935bf6f6de953e3c
Nagy Bence <nbence0620@gmail.com>
2025-10-01 18:01:57 +0200

    Change yellow text later!

--- userinterface/src/components/ExponentiationPage.js
+++ userinterface/src/components/ExponentiationPage.js
@@ -6,64 +6,64 @@ export function ExponentiationPage(){
     return(
         <div>
             <NavMenu/>
-            <div class="container border border-primary border-3 rounded-4 " style={{background:"darkgray"}}>
-                <div class="border-bottom border-5 border-primary">
+            <div class="container">
+                <div >
                     <br></br>
                     <p class="h1 display-1 text-center" style={{fontFamily:"sans-serif"}}> Choosen topic: Exponentiation </p>
                 </div>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif"}}>
                     It is recommended to review multiplication and variables first, before starting this topic.
                 </p>
                 <br></br>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif"}}>
+                <p class="h4" style={{fontFamily:"sans-serif"}}>
                     <u>
                     Let's suppose you have the following multiplication:
                     </u>
                 </p>
-                <p class="h6 display-6 text-center" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-center" style={{fontFamily:"sans-serif"}}>
                     2 &#8729; 2 &#8729; 2
                 </p>
                 <div class="text-center">
                     <img class="w-50 h-50" src={require("../assets/Exponent1.png")} alt="Image goes here." />
                 </div>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif", color:"blue"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif", color:"blue"}}>
                     Just like we did in addition we break it to multiple parts.
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif"}}>
                     <span style={{color:"yellow"}}>First</span>, 2 &#8729; 2 = 4 so we replace it.
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif"}}>
                     <span style={{color:"yellow"}}>Then</span> <span style={{color:"orange"}}>2 &#8729; 2</span> &#8729; 2 = <span style={{color:"orange"}}>4</span> &#8729; 2
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif"}}>
                 <span style={{color:"yellow"}}>Finally</span> <span style={{color:"green"}}>4 &#8729; 2 = 8</span> so the answer is <span style={{color:"green"}}>8</span>.
                 </p>
                 <div class="text-center">
                     <img class="w-50 h-50" src={require("../assets/Exponent2.png")} alt="Image goes here." />
                 </div>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif"}}>
                     Yes, we will simplify multiplication now.
                 </p>
-                <p class="h6 display-6 text-center" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-center" style={{fontFamily:"sans-serif"}}>
                     2 &#8729; 2 &#8729; 2 = <span style={{color:"green"}}> 2<sup>3</sup></span>
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif"}}>
                     The answer is still 8.
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif", color:"green"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif", color:"green"}}>
                     Let's look it closer:
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif", color:"green"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif", color:"green"}}>
                     2 : the base of exponentiation, the number we want to multiply
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif", color:"green"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif", color:"green"}}>
                     <sup>3</sup> : the exponent, the number of times we want to multiply the base with itself.
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif", color:"red"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif", color:"red"}}>
                     You may think this is useless, when I want to multiply thress a hundred times with each other, it is very confortable to write 3<sup>100</sup>,
                     then 3 &#8729; 3 &#8729; 3 &#8729; 3 &#8729;.... and so on 100 times.
                 </p>
-                <p class="h6 display-6 text-start" style={{fontFamily:"sans-serif", color:"red"}}>
+                <p class="h4 text-start" style={{fontFamily:"sans-serif", color:"red"}}>
                     Exponentiation appears in science a lot.
                 </p>
                 <a href="http://localhost:3000">Further explaination in a youtube video</a>
--- userinterface/src/components/VariablePage.js
+++ userinterface/src/components/VariablePage.js
@@ -6,52 +6,52 @@ export function VariablePage(){
     return(
         <div>
             <NavMenu/>
-            <div class="container border border-primary border-3 rounded-4 " style={{background:"darkgray"}}>
-                <div class="border-bottom border-5 border-primary">
+            <div class="container">
+                <div >
                     <br></br>
                     <p class=" h1 display-1 text-center" style={{fontFamily:"sans-serif"}}> Choosen topic: Variables </p>
             </div>
-            <p class="text-start h6 display-6 " style={{fontFamily:"sans-serif"}}>
+            <p class="text-start h4" style={{fontFamily:"sans-serif"}}>
                 It is recommended to review multiplication and division first, before starting this topic.
             </p>
             <br></br>
-            <p class="text-start h6 display-6 " style={{fontFamily:"sans-serif"}}>
+            <p class="text-start h4 " style={{fontFamily:"sans-serif"}}>
                 Variables are used to <span style={{color:"orange"}}>mark numbers</span> that we may or may not know the value of.
             </p>
-            <p class="text-start h6 display-6 " style={{fontFamily:"sans-serif", color:"blue"}}>
+            <p class="text-start h4" style={{fontFamily:"sans-serif", color:"blue"}}>
                 They are usually represented with a selected letter.
             </p>
             <div class="text-center">
                     <img class="w-50 h-50"  src={require("../assets/Variables1.png")} alt="Image goes here." />
             </div>
-             <p class="text-start h6 display-6 " style={{fontFamily:"sans-serif"}}>
+             <p class="text-start h4" style={{fontFamily:"sans-serif"}}>
                 Well when you have situations like that, it's usually the question's answer is to find that.
              </p>
-             <p class="text-start h6 display-6 " style={{fontFamily:"sans-serif"}}>
+             <p class="text-start h4" style={{fontFamily:"sans-serif"}}>
                 We will look at those situations later in the equations topic.
              </p>
-             <p class="text-start h6 display-6  border border-primary border-4 border-info" style={{fontFamily:"sans-serif"}}>
+             <p class="text-start h4  border border-primary border-4 border-info" style={{fontFamily:"sans-serif"}}>
                 But for now we discuss the situation where the variables true value is present.
              </p>
-             <p class="text-start h6 display-6 " style={{fontFamily:"sans-serif", color:"purple"}}>
+             <p class="text-start h4" style={{fontFamily:"sans-serif", color:"purple"}}>
                 Let's look at this example:
              </p>
-             <p class="text-start h6 display-6 t" style={{fontFamily:"sans-serif"}}>
+             <p class="text-start h4" style={{fontFamily:"sans-serif"}}>
                 If <span style={{color:"orange"}}>a = 1</span> what is the value of <span style={{color:"orange"}}>a + 2</span>?
              </p>
-             <p class="text-start h6 display-6 t" style={{fontFamily:"sans-serif"}}>
+             <p class="text-start h4" style={{fontFamily:"sans-serif"}}>
                 We can take the value of a which is 1 according to the = sign and replace it in the question's problem.
              </p>
-             <p class="text-center h6 display-6 " style={{fontFamily:"sans-serif"}}>
+             <p class="text-center h4" style={{fontFamily:"sans-serif"}}>
                 a + 2 = 1 + 2 = 3
              </p>
-             <p class="text-center h6 display-6 " style={{color:"green",fontFamily:"sans-serif"}}>
+             <p class="text-center h4" style={{color:"green",fontFamily:"sans-serif"}}>
                 Our final answer is 3.
              </p>
              <div class="text-center">
                     <img class="w-50 h-50" src={require("../assets/Variables2.png")} alt="Image goes here." />
             </div>
-           <p class="text-start h6 display-6 " style={{fontFamily:"sans-serif"}}>
+           <p class="text-start h4" style={{fontFamily:"sans-serif"}}>
                 Yes, it is. remember, when you know the value of a Variable you can replace the Variable with the associated number.
             </p>  
             <br></br>
